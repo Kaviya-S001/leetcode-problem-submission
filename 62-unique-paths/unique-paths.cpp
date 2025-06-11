f7f03a6d27c9a2a15adea93dfe7b80c5892edee4
Kaviya-S001 <kaviyajanani001@gmail.com>
2025-06-11 15:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (91.66%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+    for (int i = 0; i < min(m - 1, n - 1); ++i)
+        ans = ans * (m + n - 2 - i) / (i + 1);
+    return (int)ans;    
+
+    }
+};
