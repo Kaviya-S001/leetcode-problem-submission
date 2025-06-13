8aeaa7490f8da9de915af753cfaf65677dc17bd9
Kaviya-S001 <kaviyajanani001@gmail.com>
2025-06-13 15:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.27%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,11 +6,10 @@ public:
             int m = (l + r) / 2;
             if (nums[m] == target)
                 return m;
-
-            if (nums[l] <= nums[m]) {
+             if (nums[l] <= nums[m]) {
                 if (nums[l] <= target && target < nums[m])
                     r = m - 1;
-                else
+                      else
                     l = m + 1;
             } else {
                 if (nums[m] < target && target <= nums[r])
